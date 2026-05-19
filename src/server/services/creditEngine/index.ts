c226a2b64416144f698d9bd4c7f3fe2219948182
Evan Tenenbaum <evan@evanmail.com>
2026-05-18 22:23:46 -0700

    feat(credit-engine): add module barrel export

---
+++ src/server/services/creditEngine/index.ts
@@ -0,0 +1,22 @@
+export { bucketConfidence, type ConfidenceLevel } from './confidence';
+export {
+  invoiceGuardClause,
+  salesOrderGuardClause,
+  salesOrderLineGuardClause,
+  paymentGuardClause
+} from './inputGuards';
+export { scoreRevenueMomentum, type SignalResult } from './signals/revenueMomentum';
+export { scoreCashCollection } from './signals/cashCollection';
+export { scoreProfitability } from './signals/profitability';
+export { scoreDebtAging } from './signals/debtAging';
+export { scoreRepaymentVelocity } from './signals/repaymentVelocity';
+export { scoreTenureDepth } from './signals/tenureDepth';
+export {
+  aggregateOverallScore,
+  mapScoreToMultiplier,
+  type Weights,
+  type SignalScores
+} from './scoring';
+export { computeBaseAmount, median } from './base';
+export { resolveEffectiveStanceId } from './effectiveStance';
+export { isColdStartReady, type ColdStartConfig } from './coldStart';
